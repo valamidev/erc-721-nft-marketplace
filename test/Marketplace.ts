54aba62acc1aa8b7e1dd662ac694929637cc5ced
T <52665225+valamidev@users.noreply.github.com>
2022-11-16 21:07:14 +0100

    Add test for 0 fees

--- test/Marketplace.ts
+++ test/Marketplace.ts
@@ -18,8 +18,6 @@ describe.only("Marketplace", function () {
   ) {
     let bn = await ethers.provider.getBlockNumber();
 
-    console.log("Block", bn);
-
     let hash = await ethers.utils.solidityKeccak256(
       ["uint256", "address", "uint256", "address"],
       [blocknumber ?? bn, tokenAddress, id, ownerAddress]
@@ -223,6 +221,33 @@ describe.only("Marketplace", function () {
       const balanceAfterBuy = await account1.getBalance();
 
       expect(balanceAfterBuy.sub(balanceBeforeBuy)).to.equal(10 ** 5 * 0.9);
+    });
+  });
+
+  describe("Fees can be disabled", function () {
+    it("Disabled fee", async function () {
+      const { marketplace, nftToken, owner, account1, account2 } =
+        await loadFixture(deployContract);
+
+      // Get listed
+      await nftToken.connect(account1).approve(marketplace.address, 1);
+
+      await marketplace
+        .connect(account1)
+        .fixedPrice(nftToken.address, 1, 10 ** 5, 350);
+
+      const orderHash = _hash(nftToken.address, 1, account1.address, 8);
+
+      await marketplace.connect(owner).setRoyaltyFee(nftToken.address, 0);
+      await marketplace.connect(owner).updateFeePercent(0);
+
+      const balanceBeforeBuy = await account1.getBalance();
+
+      await marketplace.connect(account2).buy(orderHash, { value: 10 ** 5 });
+
+      const balanceAfterBuy = await account1.getBalance();
+
+      expect(balanceAfterBuy.sub(balanceBeforeBuy)).to.equal(10 ** 5);
     });
   });
 });
