831900cfb5459cb4f0f23094e8a4aaf12ab0a77e
T <52665225+valamidev@users.noreply.github.com>
2022-12-11 19:59:25 +0100

    Extend test contract

--- contracts/TEST/nftContract.sol
+++ contracts/TEST/nftContract.sol
@@ -6,8 +6,63 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract Token is ERC721("testToken", "tNFT") {
-    function mint(address owner, uint256 id) public {
-        console.log("Mint %s for %s", id, owner);
-        _mint(owner, id);
-    }
+	mapping(uint256 => bool) public approvalMap;
+	mapping(uint256 => bool) public transferMap;
+
+	function mint(address owner, uint256 id) public {
+		console.log("Mint %s for %s", id, owner);
+		_mint(owner, id);
+	}
+
+	function isApprovalDisabled(uint256 _tokenId) public view returns (bool) {
+		return approvalMap[_tokenId];
+	}
+
+	function isTransferDisabled(uint256 _tokenId) public view returns (bool) {
+		return transferMap[_tokenId];
+	}
+
+	modifier onlyIfApprovalEnabled(uint256 _tokenId) {
+		require(
+			!isTransferDisabled(_tokenId),
+			"Approval is disabled for this token"
+		);
+		_;
+	}
+
+	modifier onlyIfTransferEnabled(uint256 _tokenId) {
+		require(
+			!isTransferDisabled(_tokenId),
+			"Transfer is disabled for this token"
+		);
+		_;
+	}
+
+	function approve(
+		address _to,
+		uint256 _tokenId
+	) public override onlyIfApprovalEnabled(_tokenId) {
+		address owner = ERC721.ownerOf(_tokenId);
+		require(_to != owner, "ERC721: approval to current owner");
+
+		require(
+			_msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+			"ERC721: approve caller is not token owner or approved for all"
+		);
+
+		_approve(_to, _tokenId);
+	}
+
+	function safeTransferFrom(
+		address _from,
+		address _to,
+		uint256 _tokenId,
+		bytes memory _data
+	) public override onlyIfTransferEnabled(_tokenId) {
+		require(
+			_isApprovedOrOwner(_msgSender(), _tokenId),
+			"ERC721: caller is not token owner or approved"
+		);
+		_safeTransfer(_from, _to, _tokenId, _data);
+	}
 }
